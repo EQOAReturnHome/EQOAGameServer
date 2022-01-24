8904a1c0fb10ee280f695400a93e1668355445c6
Devin Dallas <devin.181991@gmail.com>
2022-01-23 22:26:38 -0800

    Made changes regarding packet processing, should be significantly more stable.

--- ReturnHome/EQOAProto-C-Sharp/Server/Network/PacketBundleFlags.cs
+++ ReturnHome/EQOAProto-C-Sharp/Server/Network/PacketBundleFlags.cs
@@ -6,11 +6,10 @@ namespace ReturnHome.Server.Network
     public enum PacketBundleFlags  : byte
     {
         NewProcessMessages        = 0x00,
-		NewProcessReport          = 0x03,
-		ProcessMessageAndReport   = 0x13,
+		RDPReport                 = 0x03,
+		ProcessUnreliableAcks     = 0x10,
         UnknownSingleByte         = 0x08,
         ProcessMessages           = 0x20,
-		ProcessReport             = 0x23,
-		ProcessAll                = 0x63
+		ProcessAck                = 0x40
     }
 }
