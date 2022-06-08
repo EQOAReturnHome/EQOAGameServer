011059eac386f04d512ae06afdc9c0a290005407
kcampbelljr <keith@fs.tech>
2022-06-08 18:24:40 -0400

    started working out spiritmaster alshan dialogue

--- ReturnHome/EQOAProto-C-Sharp/Scripts/freeport/Spiritmaster_Alshan.lua
+++ ReturnHome/EQOAProto-C-Sharp/Scripts/freeport/Spiritmaster_Alshan.lua
@@ -1,5 +1,19 @@
+local ch = tostring(choice)
 function event_say()
-diagOptions = {}
+    local diagOptions = {}
+    local questText = ""
+    local npcDialogue = ""
+    SetPlayerFlags(mySession, "EasternMagician0", true)
+    if (mySession.MyCharacter.Class == 10 and mySession.MyCharacter.Race == 0 and mySession.MyCharacter.HumTypeNum == 0 and
+    GetPlayerFlags(mySession, "EasternMagician0") and GetPlayerFlags(mySession, "EasternMagician1") == false and mySession.MyCharacter.Level >= 2)
     npcDialogue = "Would you like me to bind your spirit to this location, child?"
-SendDialogue(mySession, npcDialogue, diagOptions)
+    then
+        npcDialogue = "todo"
+        diagOptions = { "todo" }
+        if() then
+        end
+    else
+        npcDialogue = "Would you like me to bind your spirit to this location, child?"
+    end
+    SendDialogue(mySession, npcDialogue, diagOptions)
 end
